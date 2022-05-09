912fdac717212774ef79bdb66189bc666771e87b
Eugene <eugene.polyakow@gmail.com>
2022-05-09 14:41:27 +0200

    Add options screen

--- Assets/Scripts/MenuScripts/MainMenu.cs
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -7,6 +7,8 @@ public class MainMenu : MonoBehaviour
 {
     public string firstLevel;
 
+    public GameObject optionsScreen;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,12 +28,12 @@ public class MainMenu : MonoBehaviour
 
     public void OpenOptions()
     {
-
+        optionsScreen.SetActive(true);
     }
 
     public void CloseOptions()
     {
-
+        optionsScreen.SetActive(false);
     }
 
     public void QuitGame()

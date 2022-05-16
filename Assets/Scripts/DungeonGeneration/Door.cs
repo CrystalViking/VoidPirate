30d2e0171b6fc0c3c8f4ea4eadfbe65b5e8d149f
Abrafaks <abrafakspl@gmail.com>
2022-05-16 20:44:23 +0200

    Remove unconnected doors

--- Assets/Scripts/DungeonGeneration/Door.cs
+++ Assets/Scripts/DungeonGeneration/Door.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum DoorType
     {
-        
+        left, right, top, bottom
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public DoorType doorType;
 }
--- Assets/Scripts/DungeonGeneration/RoomController.cs
+++ Assets/Scripts/DungeonGeneration/RoomController.cs
@@ -87,27 +87,40 @@ public class RoomController : MonoBehaviour
 
     public void RegisterRoom(Room room)
     {
-
-        room.transform.position = new Vector3(currentLoadRoomData.X * room.Width, currentLoadRoomData.Y * room.Height, 0);
-        room.X = currentLoadRoomData.X;
-        room.Y = currentLoadRoomData.Y;
-        room.name = currentWorldName + "-" + currentLoadRoomData.name + " " + room.X + ", " + room.Y;
-        room.transform.parent = transform;
-
-        isLoadingRoom = false;
-
-        if (loadedRooms.Count == 0)
+        if (!DoesRoomExist(currentLoadRoomData.X, currentLoadRoomData.Y))
         {
-            CameraController.instance.currRoom = room;
-        }
+            room.transform.position = new Vector3(currentLoadRoomData.X * room.Width, currentLoadRoomData.Y * room.Height, 0);
+            room.X = currentLoadRoomData.X;
+            room.Y = currentLoadRoomData.Y;
+            room.name = currentWorldName + "-" + currentLoadRoomData.name + " " + room.X + ", " + room.Y;
+            room.transform.parent = transform;
 
-        loadedRooms.Add(room);
+            isLoadingRoom = false;
+
+            if (loadedRooms.Count == 0)
+            {
+                CameraController.instance.currRoom = room;
+            }
+            
+            loadedRooms.Add(room);
+            room.RemoveUnconnectedDoors();
+        }
+        else
+        {
+            Destroy(room.gameObject);
+            isLoadingRoom = false;
+        }
     }
 
 
     public bool DoesRoomExist(int x, int y)
     {
         return loadedRooms.Find(item => item.X == x && item.Y == y) != null;
+    }
+
+    public Room FindRoom(int x, int y)
+    {
+        return loadedRooms.Find(item => item.X == x && item.Y == y);
     }
 
     public void OnPlayerEnterRoom(Room room)

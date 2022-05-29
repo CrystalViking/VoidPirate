c1bd0dd4db4fa6e704c4e90984f36ee61c664c67
Eugene <eugene.polyakow@gmail.com>
2022-05-29 20:08:29 +0200

    add fire rate, add shooting on mouse hold

--- Assets/Scripts/WeaponsAndItems/WeaponShooting.cs
+++ Assets/Scripts/WeaponsAndItems/WeaponShooting.cs
@@ -7,6 +7,8 @@ public class WeaponShooting : MonoBehaviour
     public GameObject projectile;
     private Weapon currentWeapon;
 
+    private float lastShootTime = 0;
+
 
     private PlayerInventory inventory;
     private EquipmentManager manager;
@@ -33,31 +35,44 @@ public class WeaponShooting : MonoBehaviour
     void Update()
     {
         currentWeapon = inventory.GetItem(manager.CurrentlyEquippedWeapon());
-        if (Input.GetMouseButtonDown(0))
+
+
+        if(currentWeapon != null)
         {
-            if(currentWeapon != null)
+            if (currentWeapon.weaponType == WeaponType.AR)
             {
-                if(currentWeapon.weaponType != WeaponType.Melee)
+                if(Input.GetKey(KeyCode.Mouse0))
                 {
-                    Debug.Log(currentWeapon.weaponSlot);
+                    //Debug.Log(currentWeapon.weaponSlot);
                     Shoot();
                 }
             }
-           
-           
         }
+
+        
     }
 
     private void Shoot()
     {
-        GameObject spell = Instantiate(projectile, transform.position, Quaternion.identity);
-        Vector3 worldMousePosition3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 mousePos = new Vector2(worldMousePosition3D.x, worldMousePosition3D.y);
-        Vector2 myPos = transform.position;
-        Vector2 direction = (mousePos - myPos).normalized;
-        spell.GetComponent<Rigidbody2D>().velocity = direction * currentWeapon.projectileForce;
-        spell.GetComponent<TestProjectile>().damage = Random.Range(currentWeapon.minDamage, currentWeapon.maxDamage);
-        spell.transform.eulerAngles = new Vector3(0, 0, GetAngleFromVectorFloat(mousePos - myPos));
+        if(Time.time > lastShootTime + currentWeapon.fireRate)
+        {
+            lastShootTime = Time.time;
+
+
+            GameObject spell = Instantiate(projectile, transform.position, Quaternion.identity);
+            Vector3 worldMousePosition3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos = new Vector2(worldMousePosition3D.x, worldMousePosition3D.y);
+            Vector2 myPos = transform.position;
+            Vector2 direction = (mousePos - myPos).normalized;
+            spell.GetComponent<Rigidbody2D>().velocity = direction * currentWeapon.projectileForce;
+            spell.GetComponent<TestProjectile>().damage = Random.Range(currentWeapon.minDamage, currentWeapon.maxDamage);
+            spell.transform.eulerAngles = new Vector3(0, 0, GetAngleFromVectorFloat(mousePos - myPos));
+
+
+            
+        }
+
+        
 
     }
 

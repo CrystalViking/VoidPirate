4f7b76b6932ac33d255fee04d531542bd6e359ae
Eugene <eugene.polyakow@gmail.com>
2022-05-29 12:51:35 +0200

    Add wrench weapon Add EquipmentManager

--- Assets/Scripts/InverntorySystem/EquipmentManager.cs
+++ Assets/Scripts/InverntorySystem/EquipmentManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EquipmentManager : MonoBehaviour
 {
     private int currentlyEquippedWeapon = 0;
+    [SerializeField]public PlayerInventory inventory;
+    private GameObject currentWeaponObject = null;
 
     [SerializeField] Weapon defaultMeleeWeapon = null;
 
@@ -17,6 +19,38 @@ public class EquipmentManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Alpha1) && currentlyEquippedWeapon != 0)
+        {
+            UnequipWeapon();
+            EquipWeapon(inventory.GetItem(0));
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && currentlyEquippedWeapon != 1)
+        {
+            UnequipWeapon();
+            EquipWeapon(inventory.GetItem(1));
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) && currentlyEquippedWeapon != 2)
+        {
+            UnequipWeapon();
+            EquipWeapon(inventory.GetItem(2));
+        }
     }
+    
+    
+    private void InstantiateWeapon(GameObject weaponObject, int weaponSlot)
+    {
+        Weapon weapon = inventory.GetItem(weaponSlot);
+    }
+
+    private void EquipWeapon(Weapon weapon)
+    {
+        currentlyEquippedWeapon = (int)weapon.weaponSlot;
+        //currentWeaponObject = Instantiate(weapon.prefab);
+    }
+
+    private void UnequipWeapon()
+    {
+        Destroy(currentWeaponObject);
+    }
+
 }

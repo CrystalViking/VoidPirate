a4d4e758209b6aa3c9626c4bfb7f7385f6566036
Jakub Henyk <jakubhenyk@gmail.com>
2022-06-07 14:02:47 +0200

    Added rotation for Hornets

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    GameObject player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if(player.transform.position.x < transform.position.x)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+}

77696917ce329f08821c8738722a967ab27f00f3
Abrafaks <abrafakspl@gmail.com>
2022-11-19 22:23:26 +0100

    Add room scripts

---
+++ Assets/Scripts/NodeGraph/RoomNodeGraphSO.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RoomNodeGraph", menuName = "Scriptable Objects/Ship/Room Node Graph")]
+public class RoomNodeGraphSO : ScriptableObject
+{
+  [HideInInspector] public RoomNodeTypeListSO roomNodeTypeList;
+  [HideInInspector] public List<RoomNodeSO> roomNodeList = new List<RoomNodeSO>();
+  [HideInInspector] public Dictionary<string, RoomNodeSO> roomNodeDictionary = new Dictionary<string, RoomNodeSO>();
+
+
+
+
+  private void Awake()
+  {
+    LoadRoomNodeDictionary();
+  }
+
+  private void LoadRoomNodeDictionary()
+  {
+    roomNodeDictionary.Clear();
+
+    foreach (RoomNodeSO node in roomNodeList)
+    {
+      roomNodeDictionary[node.id] = node;
+    }
+  }
+
+  public RoomNodeSO GetRoomNode(string roomNodeId)
+  {
+    if (roomNodeDictionary.TryGetValue(roomNodeId, out RoomNodeSO roomNode))
+    {
+      return roomNode;
+    }
+    return null;
+  }
+
+#if UNITY_EDITOR
+  [HideInInspector] public RoomNodeSO roomNodeToDrawLineFrom = null;
+  [HideInInspector] public Vector2 linePosition;
+
+  public void OnValidate()
+  {
+    LoadRoomNodeDictionary();
+  }
+
+  public void SetNodeToDrawConnectionLineFrom(RoomNodeSO node, Vector2 position)
+  {
+    roomNodeToDrawLineFrom = node;
+    linePosition = position;
+  }
+#endif
+}

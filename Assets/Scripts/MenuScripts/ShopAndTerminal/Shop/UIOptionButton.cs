be87b4f45f9f06a31141c9f43d0b5c9b7ba5af99
Eugene <eugene.polyakow@gmail.com>
2022-11-22 22:25:10 +0100

    added new [monogram] font added new color palette: swedish palette added some UI elements sprites started working on shop/terminal window design

---
+++ Assets/Scripts/MenuScripts/ShopAndTerminal/Shop/UIOptionButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIOptionButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    //public Image prompt;
+    //public Image none;
+    Image GetImage;
+
+    void Start()
+    {
+        GetImage = GetComponentInChildren<Image>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GetImage.gameObject.SetActive(true);
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        GetImage.gameObject.SetActive(false);
+    }
+
+    
+}

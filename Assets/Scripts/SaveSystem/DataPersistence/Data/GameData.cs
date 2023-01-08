f95bfc9a808cf1f3f795cff9346bbf9c3c53ea96
Eugene <eugene.polyakow@gmail.com>
2023-01-08 23:51:08 +0100

    start working on manual save system disable coin ui updates through player prefs

---
+++ Assets/Scripts/SaveSystem/DataPersistence/Data/GameData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData 
+{
+    public int coinCount;
+    public int totalCoinCount;
+
+    
+
+    public GameData()
+    {
+        this.coinCount = 0;
+    }
+}

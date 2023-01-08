9a0008cbe3f705dde4580f7a64dca80dcd616fc8
Abrafaks <abrafakspl@gmail.com>
2023-01-08 20:47:10 +0100

    Add new variants of rooms for dungeon levels

--- Assets/Scripts/DungeonGeneration/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGeneration/DungeonGenerator.cs
@@ -7,15 +7,16 @@ public class DungeonGenerator : MonoBehaviour
     public DungeonGenerationData dungeonGenerationData;
     private List<Vector2Int> dungeonRooms;
 
-    private void Start() {
+    private void Start()
+    {
         dungeonRooms = DungeonCrawlerController.GenerateDungeon(dungeonGenerationData);
         SpawnRooms(dungeonRooms);
     }
 
     private void SpawnRooms(IEnumerable<Vector2Int> rooms)
     {
-        RoomController.instance.LoadRoom("Start", 0, 0);
-        foreach(Vector2Int roomLocation in rooms)
+        RoomController.instance.LoadRoom("Start" + RoomController.instance.currentLevel, 0, 0);
+        foreach (Vector2Int roomLocation in rooms)
         {
             RoomController.instance.LoadRoom(RoomController.instance.GetRandomRoomName(), roomLocation.x, roomLocation.y);
         }

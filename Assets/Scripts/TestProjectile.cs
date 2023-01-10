5bb80d70922d90dd5c7c467ce93ad1646b4ccf99
Jakub Henyk <jakubhenyk@gmail.com>
2023-01-10 23:20:57 +0100

    added Homing weapons

--- Assets/Scripts/TestProjectile.cs
+++ Assets/Scripts/TestProjectile.cs
@@ -34,19 +34,6 @@ public class TestProjectile : MonoBehaviour, IProjectile
                 collision.GetComponent<IEnemy>().TakeDamage(Damage);
                 Destroy(gameObject);
             }
-            //if (collision.GetComponent<EstrellaController>() != null)
-            //{
-            //    collision.GetComponent<EstrellaController>().TakeDamage(damage);
-            //}
-            //if (collision.GetComponent<ReaperController>() != null)
-            //{
-            //    collision.GetComponent<ReaperController>().TakeDamage(damage);
-            //}
-            //if (collision.GetComponent<MinionController>() != null)
-            //{
-            //    collision.GetComponent<MinionController>().TakeDamage(damage);
-            //}
-            //Destroy(gameObject);
         }
         else
         {

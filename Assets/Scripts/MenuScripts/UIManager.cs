8753e0d3e5e7cf0c6e1e200e70a048048ecd2a2d
Eugene <eugene.polyakow@gmail.com>
2023-01-11 20:56:04 +0100

    fix pause menu

--- Assets/Scripts/MenuScripts/UIManager.cs
+++ Assets/Scripts/MenuScripts/UIManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    private bool isPaused = false;
+    private bool isPaused;
 
 
     [SerializeField] private GameObject hudCanvas = null;
@@ -13,36 +13,75 @@ public class UIManager : MonoBehaviour
 
     private void Start()
     {
-        SetActiveHud(true);
+        //SetActiveHud();
+        SetPauseInactive();
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape) && !isPaused)
         {
-            SetActivePause(true);
+            //SetActivePause(true);
+            SetPauseActive();
         }
         else if(Input.GetKeyDown(KeyCode.Escape) && isPaused)
         {
-            SetActivePause(false);
-            
+            //SetActivePause(false);
+            SetPauseInactive();
         }
+        if(pauseCanvas != null)
+        {
+            if(pauseCanvas.activeSelf)
+            {
+                isPaused = true;
+            }
+            else
+            {
+                isPaused=false;
+            }
+        }
+        
     }
 
-    public void SetActiveHud(bool state)
+    public void SetActiveHud()
     {
         if(hudCanvas != null)
-            hudCanvas?.SetActive(state);
+            hudCanvas?.SetActive(true);
 
-        pauseCanvas?.SetActive(!state);
+
+        isPaused = false;
+        //pauseCanvas?.SetActive(!state);
+    }
+
+    public void SetPauseActive()
+    {
+        if (hudCanvas != null)
+            hudCanvas.SetActive(false);
+        if (pauseCanvas != null)
+            pauseCanvas.SetActive(true);
+
+        //isPaused = true;
+        Time.timeScale = 0f;
+    }
+
+    public void SetPauseInactive()
+    {
+        if (hudCanvas != null)
+            hudCanvas.SetActive(true);
+        if (pauseCanvas != null)
+            pauseCanvas.SetActive(false);
+
+        //isPaused = false;
+        Time.timeScale = 1f;
     }
 
 
     public void SetActivePause(bool state)
     {
-        if(hudCanvas != null)
+        if (hudCanvas != null)
             hudCanvas?.SetActive(!state);
-        pauseCanvas?.SetActive(state);
+        if (pauseCanvas != null)
+            pauseCanvas?.SetActive(state);
 
         Time.timeScale = state ? 0 : 1;
         isPaused = state;
@@ -57,6 +96,8 @@ public class UIManager : MonoBehaviour
 
     public void ContinueGame()
     {
-        SetActivePause(false);
+        //SetActivePause(false);
+        if(isPaused)
+            SetPauseInactive();
     }
 }
